082abd9e01324e71eb9bf61265654e8110f0862a
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-12-01 17:37:58 +0100

    Example periodic execution of a sip show peers command

--- amievents2amqp.go
+++ amievents2amqp.go
@@ -7,12 +7,14 @@ import (
 	"log"
 	"net"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
 	"encoding/json"
 	"net/url"
 
+	"github.com/aleasoluciones/goaleasoluciones/scheduledtask"
 	"github.com/aleasoluciones/simpleamqp"
 	"github.com/cenkalti/backoff"
 )
@@ -87,13 +89,11 @@ func sipShowPeers(nivisAmiURI string) (int, error) {
 	password, _ := u.User.Password()
 	user := u.User.Username()
 	hostPort := fmt.Sprintf("%s:%d", u.Host, 5038)
-
 	c, err := net.Dial("tcp", hostPort)
 	if err != nil {
 		return 0, err
 	}
 	defer c.Close()
-
 	fmt.Fprintf(c, "Action: login\r\n")
 	fmt.Fprintf(c, "Username: %s\r\n", user)
 	fmt.Fprintf(c, "Secret: %s\r\n", password)
@@ -105,21 +105,50 @@ func sipShowPeers(nivisAmiURI string) (int, error) {
 	fmt.Fprintf(c, "\r\n")
 
 	c.SetReadDeadline(time.Now().Add(AMI_TIMEOUT))
+	onlineRegexp := regexp.MustCompile(`monitored: ([\d]+) online`)
 	connbuf := bufio.NewReader(c)
+	sipUsers := false
 	for {
 		str, err := connbuf.ReadString('\n')
-		log.Println("Ami read:", strings.TrimSpace(str), err)
+		if len(str) > 0 {
+			if strings.Contains(str, "Name/username") {
+				sipUsers = true
+			} else if len(onlineRegexp.FindStringSubmatch(str)) == 2 {
+				continue
+			} else if strings.Contains(str, "--END COMMAND--") {
+				return 0, nil
+			} else if sipUsers {
+				str = strings.TrimSpace(str)
+				log.Println("Sip peer", str)
+			}
+		}
+		if err != nil {
+			return 0, err
+		}
 	}
+	return 0, nil
 }
 
 func main() {
-	var exchange, topic string
+	var exchange, topic, amiURI, amqpURI string
 	flag.StringVar(&exchange, "exchange", "events", "AMQP exchange name")
 	flag.StringVar(&topic, "topic", "astevents", "topic")
+	flag.StringVar(&amiURI, "amiURI", "", "AMI connection URI (use AMI_URI env var as default)")
+	flag.StringVar(&amqpURI, "amqpURI", "", "AMQP connection URI (use BROKER_URI env var as default)")
 	flag.Parse()
 
-	amiURI := os.Getenv("AMI_URI")
-	amqpURI := os.Getenv("BROKER_URI")
+	if amiURI == "" {
+		amiURI = os.Getenv("AMI_URI")
+	}
+	if amqpURI == "" {
+		amqpURI = os.Getenv("BROKER_URI")
+	}
+	fmt.Println("AMI_URI", amiURI)
+	fmt.Println("amqpURI", amqpURI)
+
+	if amqpURI == "" || amiURI == "" {
+		log.Panic("No amiURI or amqpURI provided")
+	}
 
 	events := make(chan (Event), 1)
 	go func() {
@@ -133,6 +162,8 @@ func main() {
 			return
 		}
 	}()
+
+	scheduledtask.NewScheduledTask(func() { sipShowPeers(amiURI) }, 60*time.Second, 0)
 
 	amqpPublisher := simpleamqp.NewAmqpPublisher(amqpURI, exchange)
 	for e := range events {
